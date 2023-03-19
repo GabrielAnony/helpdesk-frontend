747695d0ed1ef40e5bc12324cc60070f7dc79ac3
unknown <gabriel.bezerra@d2ti.com.br>
2023-03-19 14:29:53 -0300

    form e validation para login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Credenciais } from "./../../models/credenciais";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+})
+export class LoginComponent implements OnInit {
+  creds: Credenciais = {
+    email: "",
+    senha: "",
+  };
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  validaCampos(): boolean {
+    if (this.email.valid && this.senha.valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
