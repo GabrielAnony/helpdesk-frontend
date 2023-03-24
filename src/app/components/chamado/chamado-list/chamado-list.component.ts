3cd3431344b7bb6c670597d91f9c728195f16099
unknown <gabriel.bezerra@d2ti.com.br>
2023-03-24 11:35:54 -0300

    criando filtro select one radio

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -10,12 +10,8 @@ import { MatPaginator } from "@angular/material/paginator";
   styleUrls: ["./chamado-list.component.css"],
 })
 export class ChamadoListComponent implements OnInit {
-  checked = false;
-  indeterminate = false;
-  labelPosition: "before" | "after" = "after";
-  disabled = false;
-
   ELEMENT_DATA: Chamado[] = [];
+  FILTERED_DATA: Chamado[] = [];
 
   displayedColumns: string[] = [
     "id",
@@ -49,5 +45,36 @@ export class ChamadoListComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status: any): string {
+    if (status == "0") {
+      return "ABERTO";
+    } else if (status == "1") {
+      return "EM ADAMENTO";
+    } else {
+      return "ENCERRADO";
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if (prioridade == "0") {
+      return "BAIXA";
+    } else if (prioridade == "1") {
+      return "MÉDIA";
+    } else {
+      return "ALTA";
+    }
+  }
+  orderByStatus(status: any): void {
+    let list: Chamado[] = [];
+    this.ELEMENT_DATA.forEach((x) => {
+      if (x.status == status) {
+        list.push(x);
+      }
+    });
+    this.FILTERED_DATA = list;
+    this.dataSource = new MatTableDataSource<Chamado>(list);
+    this.dataSource.paginator = this.paginator;
   }
 }
